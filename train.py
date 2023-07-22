35e916b4554a5bc0e93298bbc7d49848150b1dfe
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-07-21 20:28:06 -0700

    Update train.py

--- train.py
+++ train.py
@@ -187,7 +187,7 @@ optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.01, momentum=0.9)
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=6, gamma=0.1)
 
 model_conv = train_model(model_conv, criterion, optimizer_conv,
-                         exp_lr_scheduler, num_epochs=25)
+                         exp_lr_scheduler, num_epochs=1)
 
 visualize_model(model_conv)
 
