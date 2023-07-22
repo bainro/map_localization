78169ce027891f0bb9e8f1d3b9795858f9293942
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-07-21 21:04:36 -0700

    Update train.py

--- train.py
+++ train.py
@@ -181,13 +181,13 @@ criterion = nn.MSELoss()
 # Observe that only parameters of final layer are being optimized as
 # opposed to before.
 # optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.01, momentum=0.9)
-optimizer_conv = optim.Adam(model_conv.fc.parameters(), lr=0.01)
+optimizer_conv = optim.Adam(model_conv.fc.parameters(), lr=0.1)
 
 # Decay LR by a factor of 0.1 every 7 epochs
 # exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=10, gamma=0.1)
 exp_lr_scheduler = None
 
 model_conv = train_model(model_conv, criterion, optimizer_conv,
-                         exp_lr_scheduler, num_epochs=20)
+                         exp_lr_scheduler, num_epochs=5)
 
 visualize_model(model_conv)
