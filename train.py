089eb61dc396287b1ef42c808dbb42461b105d62
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-07-22 11:01:40 -0700

    Update train.py

--- train.py
+++ train.py
@@ -46,14 +46,14 @@ def imshow(inp, title=None):
     plt.imshow(inp)
     if title is not None:
         plt.title(title)
-    plt.pause(0.01)  # pause a bit for the plots
-    plt.show()
+    plt.pause(0.001)  # pause a bit for the plots
 
 # Get a batch of training data
 inputs, _x, _y = next(iter(dataloaders['train']))
 inputs = inputs[:8,...]
 out = torchvision.utils.make_grid(inputs)
 imshow(out, title="Verify the images loaded correctly!")
+plt.show()
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs=1):
     start_time = time.time()
