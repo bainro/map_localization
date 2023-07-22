69ae0501415c9d4d1eb9bcc42d620a9c3dcd2dfb
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-07-22 14:41:44 -0700

    Update train.py

--- train.py
+++ train.py
@@ -177,7 +177,7 @@ def visualize_model(model, num_images=1):
             gt_y = gt_y * map_img.shape[0]
             plt.scatter(x=o_x, y=o_y, c=[[1.,0.,0.,1.]], s=18, label="Prediction")
             plt.scatter(x=gt_x, y=gt_y, c=[[0.,1.,0.,1.]], s=18, label="Ground Truth")
-            plt.legend(loc="lower right", fontsize=8, frameon=False, bbox_to_anchor=(1.0, 0.0), labelspacing=0.35)
+            plt.legend(loc="lower right", fontsize=8, frameon=False, bbox_to_anchor=(1.0, -0.1), labelspacing=0.35)
             
             plt.pause(0.001)
             plt.clf()
