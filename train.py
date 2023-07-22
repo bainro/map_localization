0cd6a2064c21ff38dccd351f70f75610ddea860e
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-07-21 19:35:55 -0700

    Update train.py

--- train.py
+++ train.py
@@ -24,13 +24,11 @@ print("Download the data from https://download.pytorch.org/tutorial/hymenoptera_
 train_trans = transforms.Compose([
     transforms.RandomResizedCrop(224),
     transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
 val_trans = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
-    transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
 
