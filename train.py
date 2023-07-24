cfe4e0066fa07727171f7a0fcc82a26a8a68b5e6
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-07-24 00:33:36 -0700

    Update train.py

--- train.py
+++ train.py
@@ -27,11 +27,11 @@ val_trans = T.Compose([
 
 image_datasets = {}
 image_datasets['train'] = LocalizeDataset('./data/nongen', train=True, transform=train_trans, 
-                                          shuffle=True, split=0.85)
+                                          shuffle=False, split=0.85)
 image_datasets['val'] = LocalizeDataset('./data/nongen', train=False, transform=val_trans,
-                                        shuffle=True, split=0.85)
+                                        shuffle=False, split=0.85)
 dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=64,
-                                             shuffle=True, num_workers=4)
+                                             shuffle=False, num_workers=4)
               for x in ['train', 'val']}
 dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
 
@@ -119,6 +119,47 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=1):
     model.load_state_dict(torch.load(best_model_params_path))
     return model
 
+def make_movie(model):
+    map_img = plt.imread('./data/2nd_blueprint.png')
+    fig = plt.figure()
+    img_id = 0
+
+    with torch.no_grad():
+        for i, (inputs, x, y) in enumerate(dataloaders['train']):
+            for j in range(inputs.size()[0]):
+                gs = GridSpec(1,9) # 1 rows, 9 columns
+                ax = fig.add_subplot(gs[0,:2])
+                ax.axis('off')
+                o_x, o_y = outputs[0].cpu().tolist()
+                gt_x, gt_y = labels[0].cpu().tolist()
+                ax.set_title("First Person View", fontsize=8)
+                imshow(inputs.cpu().data[j])
+    
+                ax = fig.add_subplot(gs[0,2:])
+                ax.axis('off')
+                o_x, o_y = outputs[j].cpu().tolist()
+                gt_x, gt_y = labels[j].cpu().tolist()
+                ax.imshow(map_img, 
+                           resample=False, 
+                           interpolation='none', 
+                           cmap='gray', 
+                           vmin=0, 
+                           vmax=255)
+    
+                # denormalize (i.e. convert back to px space)
+                o_x = o_x * map_img.shape[1]
+                o_y = o_y * map_img.shape[0]
+                gt_x = gt_x * map_img.shape[1]
+                gt_y = gt_y * map_img.shape[0]
+                plt.scatter(x=o_x, y=o_y, c=[[1.,0.,0.,1.]], s=11, label="Prediction")
+                plt.scatter(x=gt_x, y=gt_y, c=[[0.,1.,0.,1.]], s=11, label="Ground Truth")
+                plt.legend(loc="lower right", fontsize=6, frameon=False, bbox_to_anchor=(1.0, -0.05), labelspacing=0.25)
+                fig.savefig(f'/tmp/{img_id:05d}.png', dpi=1200, bbox_inches='tight') # format='svg'
+                img_id += 1
+                # plt.pause(0.001)
+                plt.clf()
+
+
 def visualize_model(model, num_images=16):
     was_training = model.training
     model.eval()
@@ -126,10 +167,6 @@ def visualize_model(model, num_images=16):
     fig = plt.figure()
 
     with torch.no_grad():
-        map_img = plt.imread('./data/2nd_blueprint.png')
-        img_id = 0
-        for i, (inputs, x, y) in enumerate(dataloaders['train']):
-        '''
         for i, (inputs, x, y) in enumerate(dataloaders['val']):
             inputs = inputs.to(device)
             x = x.to(device)
@@ -153,44 +190,6 @@ def visualize_model(model, num_images=16):
                 if images_so_far == num_images:
                     model.train(mode=was_training)
                     return
-           
-        '''
-            
-        for j in range(inputs.size()[0]):
-            gs = GridSpec(1,9) # 1 rows, 9 columns
-            ax = fig.add_subplot(gs[0,:2])
-            # ax = plt.subplot(1, 2, 1)
-            ax.axis('off')
-            o_x, o_y = outputs[0].cpu().tolist()
-            gt_x, gt_y = labels[0].cpu().tolist()
-            ax.set_title("First Person View", fontsize=8)
-            imshow(inputs.cpu().data[j])
-
-            ax = fig.add_subplot(gs[0,2:])
-            # ax = plt.subplot(1, 2, 2)
-            ax.axis('off')
-            o_x, o_y = outputs[j].cpu().tolist()
-            gt_x, gt_y = labels[j].cpu().tolist()
-            ax.imshow(map_img, 
-                       resample=False, 
-                       interpolation='none', 
-                       cmap='gray', 
-                       vmin=0, 
-                       vmax=255)
-
-            # denormalize (i.e. convert back to px space)
-            o_x = o_x * map_img.shape[1]
-            o_y = o_y * map_img.shape[0]
-            gt_x = gt_x * map_img.shape[1]
-            gt_y = gt_y * map_img.shape[0]
-            plt.scatter(x=o_x, y=o_y, c=[[1.,0.,0.,1.]], s=11, label="Prediction")
-            plt.scatter(x=gt_x, y=gt_y, c=[[0.,1.,0.,1.]], s=11, label="Ground Truth")
-            plt.legend(loc="lower right", fontsize=6, frameon=False, bbox_to_anchor=(1.0, -0.05), labelspacing=0.25)
-            fig.savefig(f'/tmp/{img_id:05d}.png', dpi=1200, bbox_inches='tight') # format='svg'
-            img_id += 1
-            # plt.pause(0.001)
-            plt.clf()
-            #'''
                 
         model.train(mode=was_training)
 
@@ -213,7 +212,8 @@ criterion = weighted_mse_loss
 optimizer_conv = optim.SGD(model_conv.parameters(), lr=0.1, momentum=0.9)
 lr_schedule = lr_scheduler.StepLR(optimizer_conv, step_size=40, gamma=0.3)
 model_conv = train_model(model_conv, criterion, optimizer_conv,
-                         lr_schedule, num_epochs=121)
+                         lr_schedule, num_epochs=0)
 
 visualize_model(model_conv)
+make_movie(model_conv)
 plt.show()
