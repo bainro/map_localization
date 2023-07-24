8d064b0966d30623f3611b016fddbd92b5a478cc
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-07-23 21:21:27 -0700

    Update train.py

--- train.py
+++ train.py
@@ -200,9 +200,8 @@ model_conv = model_conv.to(device)
 
 def weighted_mse_loss(input, target):
     weight = torch.ones_like(input)
-    # scale just the x values
     #@TODO make this a CLI arg
-    weight[:,0] = weight[:,0] / 3.4 # from map's aspect ratio
+    weight[:,1] = weight[:,1] / 3.4 # from map's aspect ratio
     return torch.mean(weight * (input - target) ** 2)
 
 criterion = weighted_mse_loss
